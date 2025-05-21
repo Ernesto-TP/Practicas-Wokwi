c6a88d7d0bfefce2389954c6488d3211a97d2bf9
Ernesto Torres Pineda <l22211665@tectijuana.edu.mx>
2025-05-21 14:30:13 -0700

    Update main.py

--- Codigo #9/main.py
+++ Codigo #9/main.py
@@ -1,1 +1,28 @@
+from machine import Pin, I2C, time_pulse_us
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+import time
 
+# LCD
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+lcd = I2cLcd(i2c, i2c.scan()[0], 2, 16)
+
+# HC-SR04
+trig = Pin(2, Pin.OUT)
+echo = Pin(3, Pin.IN)
+
+def medir_distancia():
+    trig.low()
+    time.sleep_us(2)
+    trig.high()
+    time.sleep_us(10)
+    trig.low()
+    duracion = time_pulse_us(echo, 1, 30000)  # timeout en µs
+    distancia = duracion * 0.0343 / 2
+    return distancia
+
+while True:
+    d = medir_distancia()
+    lcd.clear()
+    lcd.putstr("Distancia:\n{:.1f} cm".format(d))
+    time.sleep(0.5)
